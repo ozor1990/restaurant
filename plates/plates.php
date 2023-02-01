89eed49342fd32f3219f8c8d9ae6f6fd34537829
ozor1990 <98599727+ozor1990@users.noreply.github.com>
2023-02-01 20:19:24 +0200

    Update plates.php

--- plates/plates.php
+++ plates/plates.php
@@ -9,28 +9,39 @@
 
 
 
-<link href="../bootstrap/css/bootstrap.css" rel="stylesheet"> 
-<link href="../bootstrap/css/mystyle.css" rel="stylesheet"> 
-
+<link href="../bootstrap/css2/animation.css" rel="stylesheet">  
+<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
 
 <style type="text/css">
 
-#plates {border:0px inset #ff4500;
+
+body {background-image: url("../webimages/rest4.jpg");
+background-position: center;
+background-repeat: no-repeat;
+color: white;
+font-weight: bold;
+text-transform: capitalize;}
+
+
+
+#plates {border:7px inset #ff4500;
    margin: auto;
   width: 100%;
- margin-top: 10%;
+ margin-top: 1%;
   padding: 10px;
 font-family: serif;
 border-radius: 30px;
-opacity: 1;}
+opacity: 1;
+}
 
 
-#first-menu-div-col-plates {border: 1px inset #ff4500;padding: 0px;
+#first-menu-div-col-plates {border: 0px inset #ff4500;padding: 0px;
 text-align: center; cursor: pointer;
 font-size: 23px;
 letter-spacing: 0.5em;
 text-shadow:2px 2px #ff4500;
 text-align: center;
+
 
 }  
 
@@ -39,38 +50,80 @@ text-align: center;
 #first-menu-div-col-plates img:hover {background-color: none;opacity: 0.8;}
 
 
-.plates-images { border-radius: 0px;
+.plates-images { border-radius: 50px;
 				padding:none;opacity: 0.8;
-				max-width: 100%;
-				max-height: 100%;
+				width: 100%;
+				height: 100%;
 				margin: auto;
 				display: block;}
 
 #plate-image-details {letter-spacing: 0;
-text-shadow:none; font-size: 16px;}	
+text-shadow:2px 2px black; font-size: 18px;}
+
+#plate-image-details:hover {background-color: black;}	
 </style>
 </head>
 
 <body>
-
+<div class="container">
 <div id="plates" class="row">
+
+
 <div id="first-menu-div-col-plates" class="col-sm-4 col-md-4 col-lg-4">
-<img class="plates-images" src="plates/plates-images/burger1.png">
+<img class="plates-images" src="plates-images/burger1.png">
 </div>
+
 <div id="first-menu-div-col-plates" class="col-sm-8 col-md-8 col-lg-8">
 <span id="plate-image-headline">1- burger plate</span> 
 <br>
-<span id="plate-image-details">our burger is the best </span>
+<span id="plate-image-details">What is a burger description?
+A burger is a patty of ground beef
+ grilled and placed between two halves of a bun. Slices of raw onion, 
+lettuce, bacon, mayonnaise, and other ingredients add flavor.</span>
 </div>
+
+
 </div>
 
 
+<!-- ending row of plates  -->
 
 
+<div id="plates" class="row">
+
+<div id="first-menu-div-col-plates" class="col-sm-4 col-md-4 col-lg-4">
+<img class="plates-images" src="plates-images/fajita1.jpg">
+</div>
+
+<div id="first-menu-div-col-plates" class="col-sm-8 col-md-8 col-lg-8">
+<span id="plate-image-headline">2-  Chicken Fajitas</span> 
+<br>
+<span id="plate-image-details">A pick-and-mix fajita sharing platter
+ that the whole family will love! Chicken Fajitas are a quick ....</span>
+</div>
 
 
+</div>
 
 
+<div id="plates" class="row">
+
+<div id="first-menu-div-col-plates" class="col-sm-4 col-md-4 col-lg-4">
+<img class="plates-images" src="plates-images/fajita1.jpg">
+</div>
+
+<div id="first-menu-div-col-plates" class="col-sm-8 col-md-8 col-lg-8">
+<span id="plate-image-headline">2-  Chicken Fajitas</span> 
+<br>
+<span id="plate-image-details">A pick-and-mix fajita sharing platter
+ that the whole family will love! Chicken Fajitas are a quick ....</span>
+</div>
+
+
+</div>
+
+
+</div>
 
 </body>
 </html>
