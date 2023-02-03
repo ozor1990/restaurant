4fac761c58190cb051d13d7ac31d89de972fcb05
ozor1990 <98599727+ozor1990@users.noreply.github.com>
2023-02-03 21:59:53 +0200

    Add files via upload

---
+++ social-media/social-media-icons.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>WebDev</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+
+<link href="../bootstrap/css/bootstrap.css" rel="stylesheet"> 
+<link href="../bootstrap/css/mystyle.css" rel="stylesheet"> 
+
+
+<style type="text/css">
+
+#social-media-big-row {position: relative; border:0px solid black;}
+
+#social-media-col-1 {border:0px solid red;
+					 text-align: center;
+					width: 33.3%;
+						cursor: pointer; 
+						position:relative;
+						opacity:;
+					}
+#span-social {background-color: #ff4500;border-radius: 100px;
+			width: 100px;height: 100px;opacity: 0.9	; margin: 10px;
+			padding: 0px;background: rgba(255, 69, 0 , 0.6);
+			border: 5px inset white;}
+
+#social-headline {font-style: italic;font-family: "Sofia", sans-serif;}								
+</style>
+
+</head>
+
+
+<body>
+<div class="row" id="social-media-big-row">
+
+
+<div id="social-media-col-1" class="col-sm-4 col-md-4 col-lg-4">
+<img src="social-media/social-media/facebook.png" id="span-social"/>
+<br>
+<span id="social-headline">facebook	</span>
+</div>
+
+<div id="social-media-col-1" class="col-sm-4 col-md-4 col-lg-4">
+<img src="social-media/social-media/insta.png" id="span-social"/>
+<br>
+<span id="social-headline">instagram	</span>
+</div>
+
+
+<div id="social-media-col-1" class="col-sm-4 col-md-4 col-lg-4">
+<img src="social-media/social-media/tiktok.png" id="span-social"/>
+<br>
+<span id="social-headline">tiktok	</span>
+</div>
+
+
+
+
+
+
+
+
+</div> <!-- end of row  -->
+
+</body>
+</html>
